5f429801316cd3545ce37d358f9437e781ce5458
Veenkar <veenkar@gmail.com>
2022-07-17 19:19:25 +0200

    Adapted sdkconfig and spi_lcd.c to Wrover Kit v4.1

--- components/prboom-esp32-compat/spi_lcd.c
+++ components/prboom-esp32-compat/spi_lcd.c
@@ -87,29 +87,61 @@ static const ili_init_cmd_t ili_init_cmds[]={
 
 
 static const ili_init_cmd_t ili_init_cmds[]={
+        /* Power contorl B, power control = 0, DC_ENA = 1 */
     {0xCF, {0x00, 0x83, 0X30}, 3},
+    /* Power on sequence control,
+     * cp1 keeps 1 frame, 1st frame enable
+     * vcl = 0, ddvdh=3, vgh=1, vgl=2
+     * DDVDH_ENH=1
+     */
     {0xED, {0x64, 0x03, 0X12, 0X81}, 4},
+    /* Driver timing control A,
+     * non-overlap=default +1
+     * EQ=default - 1, CR=default
+     * pre-charge=default - 1
+     */
     {0xE8, {0x85, 0x01, 0x79}, 3},
+    /* Power control A, Vcore=1.6V, DDVDH=5.6V */
     {0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02}, 5},
+    /* Pump ratio control, DDVDH=2xVCl */
     {0xF7, {0x20}, 1},
+    /* Driver timing control, all=0 unit */
     {0xEA, {0x00, 0x00}, 2},
+    /* Power control 1, GVDD=4.75V */
     {0xC0, {0x26}, 1},
+    /* Power control 2, DDVDH=VCl*2, VGH=VCl*7, VGL=-VCl*3 */
     {0xC1, {0x11}, 1},
+    /* VCOM control 1, VCOMH=4.025V, VCOML=-0.950V */
     {0xC5, {0x35, 0x3E}, 2},
+    /* VCOM control 2, VCOMH=VMH-2, VCOML=VML-2 */
     {0xC7, {0xBE}, 1},
+    /* Memory access contorl, MX=MY=0, MV=1, ML=0, BGR=1, MH=0 */
     {0x36, {0x28}, 1},
+    /* Pixel format, 16bits/pixel for RGB/MCU interface */
     {0x3A, {0x55}, 1},
+    /* Frame rate control, f=fosc, 70Hz fps */
     {0xB1, {0x00, 0x1B}, 2},
+    /* Enable 3G, disabled */
     {0xF2, {0x08}, 1},
+    /* Gamma set, curve 1 */
     {0x26, {0x01}, 1},
+    /* Positive gamma correction */
     {0xE0, {0x1F, 0x1A, 0x18, 0x0A, 0x0F, 0x06, 0x45, 0X87, 0x32, 0x0A, 0x07, 0x02, 0x07, 0x05, 0x00}, 15},
+    /* Negative gamma correction */
     {0XE1, {0x00, 0x25, 0x27, 0x05, 0x10, 0x09, 0x3A, 0x78, 0x4D, 0x05, 0x18, 0x0D, 0x38, 0x3A, 0x1F}, 15},
+    /* Column address set, SC=0, EC=0xEF */
     {0x2A, {0x00, 0x00, 0x00, 0xEF}, 4},
-    {0x2B, {0x00, 0x00, 0x01, 0x3f}, 4}, 
+    /* Page address set, SP=0, EP=0x013F */
+    {0x2B, {0x00, 0x00, 0x01, 0x3f}, 4},
+    /* Memory write */
     {0x2C, {0}, 0},
+    /* Entry mode set, Low vol detect disabled, normal display */
     {0xB7, {0x07}, 1},
+    /* Display function control */
     {0xB6, {0x0A, 0x82, 0x27, 0x00}, 4},
+    /* Sleep out */
     {0x11, {0}, 0x80},
+    /* Display on */
     {0x29, {0}, 0x80},
     {0, {0}, 0xff},
 };
@@ -161,7 +193,7 @@ void ili_init(spi_device_handle_t spi)
     //Initialize non-SPI GPIOs
     gpio_set_direction(PIN_NUM_DC, GPIO_MODE_OUTPUT);
     gpio_set_direction(PIN_NUM_RST, GPIO_MODE_OUTPUT);
-    //gpio_set_direction(PIN_NUM_BCKL, GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_NUM_BCKL, GPIO_MODE_OUTPUT);
 
     //Reset the display
     gpio_set_level(PIN_NUM_RST, 0);
@@ -290,17 +322,19 @@ void IRAM_ATTR displayTask(void *arg) {
         .spics_io_num=PIN_NUM_CS,               //CS pin
         .queue_size=NO_SIM_TRANS,               //We want to be able to queue this many transfers
         .pre_cb=ili_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+        .flags=SPI_DEVICE_NO_DUMMY,             // Specify to ignore frequency limit of 26.7MHz
     };
+
 
 	printf("*** Display task starting.\n");
 
     //heap_caps_print_heap_info(MALLOC_CAP_DMA);
 
     //Initialize the SPI bus
-    ret=spi_bus_initialize(VSPI_HOST, &buscfg, 2);  // DMA Channel
+    ret=spi_bus_initialize(HSPI_HOST, &buscfg, 2);  // DMA Channel
     assert(ret==ESP_OK);
     //Attach the LCD to the SPI bus
-    ret=spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
+    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
     assert(ret==ESP_OK);
     //Initialize the LCD
     ili_init(spi);
