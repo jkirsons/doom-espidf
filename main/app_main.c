a892388bbdf48bb1dba4b625c47f759a248a0975
Veenkar <veenkar@gmail.com>
2022-07-17 19:20:26 +0200

    Modified main to test LCD

--- main/app_main.c
+++ main/app_main.c
@@ -29,17 +29,25 @@
 #include "freertos/task.h"
 #include <stdlib.h>
 #include "esp_err.h"
+#include <stdint.h>
 //#include "nvs_flash.h"
 //#include "esp_partition.h"
 
 //#include "i_system.h"
 
-//#include "spi_lcd.h"
+#include "spi_lcd.h"
+#include "i_video.h"
+
+
+#define POPE_COLOR 			0xFC
+#define POPE_COLOR_ALT 		0x03
 
 
 extern void jsInit();
 extern int doom_main(int argc, char const * const *argv);
 extern void spi_lcd_init() ;
+
+void updateTestScreen();
 
 void doomEngineTask(void *pvParameters)
 {
@@ -47,9 +55,100 @@ void doomEngineTask(void *pvParameters)
     doom_main(3, argv);
 }
 
+static uint8_t test_text[24][32] = 
+{
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0},
+	{0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+	{0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0},
+	{0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+	{0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+	{0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+	{0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+	{0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+static uint8_t disp[240][320];
+
+void updateTestScreen()
+{
+	static uint8_t color = POPE_COLOR;
+	for (size_t row = 0; row < 240; ++row)
+	{
+		for (size_t col = 0; col < 320; ++col)
+		{
+			if (test_text[row/10][col/10])
+			{
+				disp[row][col] = color;
+			}
+		}
+	}
+	if (color == POPE_COLOR)
+	{
+		color = POPE_COLOR_ALT;
+	}
+	else
+	{
+		color = POPE_COLOR;
+	}
+}
+
+void dummyTask(void *pvParameters)
+{
+	while (1)
+	{
+		static uint8_t counter;
+		spi_lcd_wait_finish();
+		spi_lcd_send(disp);
+		if (!(counter++))
+		{
+			updateTestScreen();
+		}
+		if (counter>=32)
+		{
+			counter = 0;
+		}
+	}
+}
+
+extern int16_t lcdpal[256];
+
+uint8_t levels_b[] = {0, 10, 20, 31};
+uint8_t levels_g[] = {0, 9, 18, 27, 36, 45, 54, 63};
+uint8_t levels_r[] = {0, 4,  8, 12, 16, 20, 24, 31};
+
+
+
 void app_main()
 {
 	spi_lcd_init();
 	jsInit();
-	xTaskCreatePinnedToCore(&doomEngineTask, "doomEngine", 18000, NULL, 5, NULL, 0);
+	// xTaskCreatePinnedToCore(&doomEngineTask, "doomEngine", 18000, NULL, 5, NULL, 0);
+	// doomEngineTask(NULL);
+
+	for (size_t pal_id=0u; pal_id<sizeof(lcdpal); ++pal_id)
+	{
+		/* create simple palette mapping RGB332 to RGB565 */
+		int16_t val = ( levels_r[(pal_id & 0xE0)>>5] << 11) | (levels_g[(pal_id & 0x1C)>>2]<<5) | (levels_b[(pal_id & 0x3)]);
+		lcdpal[pal_id] = ((val & 0xFF) << 8) | ((val & 0xFF00) >> 8);
+	}
+
+	updateTestScreen(POPE_COLOR);
+
+	xTaskCreatePinnedToCore(&dummyTask, "dummyTask", 18000, NULL, 5, NULL, 0);
 }
